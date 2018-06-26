bc635041fa93a0d544b86ab7aedf8c091542e8c6
Hanna Bonert <hbonert@csumb.edu>
2018-06-26 17:16:07 +0000

    index.php updated with lab 2

--- index.php
+++ index.php
@@ -5,13 +5,15 @@
 // then click the URL that is emitted to the Output tab of the console
 
 echo 'Hanna Bonert- CST336 Internet Programming Class';
-echo "<br>";
+echo "<br><br>";
 echo 'Labs';
-echo "<br>";
+echo "<br><br>";
 echo '<a href="labs/lab1/index.html">Lab 1</a>';
 echo "<br>";
+echo '<a href="labs/lab2/777/index.php">Lab 2</a>';
+echo "<br><br>";
 echo 'Homeworks';
-echo "<br>";
+echo "<br><br>";
 echo '<a href="hw/homework1/index.html">Homework 1</a>';
 ?>
 </body>
