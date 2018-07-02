91a22350d9b307eb2128f8acbc926463c39f5715
Hanna Bonert <hbonert@csumb.edu>
2018-07-02 21:36:04 +0000

    updated index.php

--- labs/lab3/Slider/index.php
+++ labs/lab3/Slider/index.php
@@ -112,9 +112,6 @@
             <input type="submit" value="Search" />
         </form>
         <br/><br />
-        <?php 
-            echo "You have selected :".$_GET['keyword'];  
-        ?>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
     </body>
