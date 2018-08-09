e737d1c2610737e96468c4a9d36d70c3f6156baa
hannabonert <hannabonert@gmail.com>
2018-08-10 01:07:55 +0300

    updated dbconnection.php for my heroku

--- cst336Final/inc/dbConnection.php
+++ cst336Final/inc/dbConnection.php
@@ -1,9 +1,9 @@
 <?php
 
-function getDBConnection($dbname = 'heroku_35ebda6bd9915d8'){
+function getDBConnection($dbname = 'heroku_fed8fc06a1d5dae'){
     $host = 'us-cdbr-iron-east-04.cleardb.net';
-    $username = 'bfaf3ab5961d91';
-    $passowrd = '52958950';
+    $username = 'b51a32c5f731cc';
+    $passowrd = '8a7c33a6';
     
     $dbConn = new PDO("mysql:host=$host; dbname=$dbname", $username, $passowrd);
     $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
